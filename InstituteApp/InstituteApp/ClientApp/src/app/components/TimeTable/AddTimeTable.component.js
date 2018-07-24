ba2934bca72e86e4fb5556a431fe884216d14e69
zahidtanveer <zahid_tanveer@live.com>
2018-07-24 17:05:17 +0500

    07/24/2018

--- InstituteApp/InstituteApp/ClientApp/src/app/components/TimeTable/AddTimeTable.component.js
+++ InstituteApp/InstituteApp/ClientApp/src/app/components/TimeTable/AddTimeTable.component.js
@@ -14,7 +14,10 @@ var createTimeTable = /** @class */ (function () {
         this._router = _router;
         this._dataService = _dataService;
         this._employeeService = _employeeService;
+        this.periodList = [];
         this.title = "Create";
+        this.DayName = "";
+        this.newList = [];
         this.timeTableForm = this._fb.group({
             id: 0,
             CourseId: ['', [forms_1.Validators.required]],
@@ -22,6 +25,8 @@ var createTimeTable = /** @class */ (function () {
             Name: ['', [forms_1.Validators.required]],
             WeekDays: ['', [forms_1.Validators.required]]
         });
+        this.periodId = 1;
+        this.getTeachers();
         this.getCourse();
         this.getBatch();
     }
@@ -40,22 +45,64 @@ var createTimeTable = /** @class */ (function () {
         this._dataService.getCourse()
             .subscribe(function (data) { _this.courseList = data; });
     };
-    createTimeTable.prototype.onModalHidden = function () {
-    };
+    //On Course Selection 
     createTimeTable.prototype.OnCourseSelection = function ($event) {
         var courseID = this.timeTableForm.controls["CourseId"].value;
         if (courseID) {
             this.subBatchList = this.batchList.filter(function (x) { return x.courseId == courseID && x != null; });
         }
     };
+    //Select and set Week Days
     createTimeTable.prototype.SetWeekDays = function () {
         this.weekDayModal.show();
     };
+    //Show Period Model PopUp
+    createTimeTable.prototype.ShowPeriodModel = function (day) {
+        this.DayName = day;
+        this.periodModal.show();
+        if (this.newList) {
+            this.newList = this.periodList.filter(function (x) { return x.day == day && x != null; });
+            console.log(this.newList);
+        }
+    };
+    //Add New Period
+    createTimeTable.prototype.AddPeriod = function () {
+        var name = document.getElementById('Name').value;
+        var startTime = document.getElementById('StartTime').value;
+        var endTime = document.getElementById('EndTime').value;
+        var teacher = document.getElementById('Teacher').value;
+        var day = this.DayName;
+        var ID = this.periodId;
+        this.newList.push({
+            ID: ID,
+            day: day,
+            name: name,
+            startTime: startTime,
+            endTime: endTime,
+            teacher: teacher
+        });
+        this.periodId++;
+        console.log(this.newList);
+    };
+    // Generate Time Table
+    createTimeTable.prototype.CreateTimeTable = function () {
+        this.periodList = this.periodList.concat(this.newList);
+        console.log(this.periodList);
+    };
     createTimeTable.prototype.save = function () {
+        console.log("Save Btn is Clicked");
+        this.weekDayModal.hide();
+        var list = [];
+        $('input[type="checkbox"]:checked').each(function () {
+            console.log(this.id);
+            list.push(this.id);
+        });
+        this.daysList = list;
         if (!this.timeTableForm.valid) {
             return;
         }
         if (this.title == "Create") {
+            console.log("Create");
             //this._subjectAllocationService.saveSubjectAllocation(this.timeTableForm.value)
             //    .subscribe((data) => {
             //        this._router.navigate(['/timeTable']);
@@ -64,6 +111,28 @@ var createTimeTable = /** @class */ (function () {
     };
     createTimeTable.prototype.cancel = function () {
         this._router.navigate(['/timeTable']);
+    };
+    createTimeTable.prototype.onFacultyModel = function (id) {
+        this.SlotId = id;
+        this.selectFacultyModal.show();
+    };
+    createTimeTable.prototype.AssignFaculty = function () {
+        var _this = this;
+        var filterPeriodList = this.periodList.filter(function (x) { return x.ID == _this.SlotId; });
+        console.log(filterPeriodList);
+        console.log(filterPeriodList[0].ID);
+        var id = filterPeriodList[0].ID;
+        if (id) {
+            var teacher = document.getElementById('Teacher').value;
+            this.periodList[id].teacher = teacher;
+            document.getElementById('Teacher').value = "";
+        }
+    };
+    createTimeTable.prototype.onDaysModalHidden = function () {
+    };
+    createTimeTable.prototype.onPeriodModalHidden = function () {
+    };
+    createTimeTable.prototype.onFacultyModalHidden = function () {
     };
     Object.defineProperty(createTimeTable.prototype, "CourseId", {
         get: function () { return this.timeTableForm.get('CourseId'); },
@@ -86,8 +155,14 @@ var createTimeTable = /** @class */ (function () {
         configurable: true
     });
     __decorate([
+        core_1.ViewChild('periodModal')
+    ], createTimeTable.prototype, "periodModal", void 0);
+    __decorate([
         core_1.ViewChild('weekDayModal')
     ], createTimeTable.prototype, "weekDayModal", void 0);
+    __decorate([
+        core_1.ViewChild('selectFacultyModal')
+    ], createTimeTable.prototype, "selectFacultyModal", void 0);
     createTimeTable = __decorate([
         core_1.Component({
             selector: 'createTimeTable',
--- InstituteApp/InstituteApp/ClientApp/src/app/components/TimeTable/AddTimeTable.component.ts
+++ InstituteApp/InstituteApp/ClientApp/src/app/components/TimeTable/AddTimeTable.component.ts
@@ -17,13 +17,23 @@ export class createTimeTable {
     public courseList: CourseData[];
     public batchList: BatchData[];
     public subBatchList: BatchData[];
-
+    daysList: any;
+    periodList=[];
     title: string = "Create";
     timeTableForm: FormGroup;
     errorMessage: any;
+    DayName: string = "";
+    newList = [];
+    periodId: number;
+    SlotId: number;
+    @ViewChild('periodModal')
+    periodModal: ModalDirective;
 
     @ViewChild('weekDayModal')
     weekDayModal: ModalDirective;
+
+    @ViewChild('selectFacultyModal')
+    selectFacultyModal: ModalDirective;
 
     constructor(private _fb: FormBuilder, private _router: Router,
         private _dataService: DataService, private _employeeService: EmployeeService) {
@@ -35,8 +45,9 @@ export class createTimeTable {
             Name: ['', [Validators.required]],
             WeekDays: ['', [Validators.required]]
         })
-      
-       
+
+        this.periodId = 1;
+        this.getTeachers();
         this.getCourse();
         this.getBatch();
        
@@ -56,9 +67,7 @@ export class createTimeTable {
             .subscribe(data => { this.courseList = data });
     }
 
-    onModalHidden() {
-
-    }
+    //On Course Selection 
     OnCourseSelection($event: any) {
 
         var courseID = this.timeTableForm.controls["CourseId"].value;
@@ -66,19 +75,68 @@ export class createTimeTable {
             this.subBatchList = this.batchList.filter(x => x.courseId == courseID && x != null);
         }
     }
+    //Select and set Week Days
     SetWeekDays() {
-
         this.weekDayModal.show();
+    }
+    //Show Period Model PopUp
+    ShowPeriodModel(day) {
+        this.DayName = day;
+        this.periodModal.show();
+        if (this.newList) {
+
+            this.newList = this.periodList.filter(x => x.day == day && x != null);
+            console.log(this.newList);
+        }
 
     }
+    //Add New Period
+    AddPeriod() {
+        
+
+        var name = (<HTMLInputElement>document.getElementById('Name')).value;
+        
+        var startTime = (<HTMLInputElement>document.getElementById('StartTime')).value;
+
+        var endTime = (<HTMLInputElement>document.getElementById('EndTime')).value;
+        var teacher = (<HTMLInputElement>document.getElementById('Teacher')).value;
+        var day = this.DayName;
+        var ID = this.periodId;
+        this.newList.push({
+            ID,
+            day,
+            name,
+            startTime,
+            endTime,
+            teacher
+        })
+        this.periodId++;
+        
+        console.log(this.newList);
+    }
+    // Generate Time Table
+    CreateTimeTable() {
+        this.periodList = this.periodList.concat(this.newList);
+        console.log(this.periodList);
+    }
+
     save() {
+        console.log("Save Btn is Clicked");
+        this.weekDayModal.hide();
+        var list=[];       
+        $('input[type="checkbox"]:checked').each(function () {
+            console.log(this.id);
+            list.push(this.id);
+        });
+        this.daysList = list;
+     
         if (!this.timeTableForm.valid) {
             return;
         }
         if (this.title == "Create") {
 
 
-
+            console.log("Create");
             //this._subjectAllocationService.saveSubjectAllocation(this.timeTableForm.value)
             //    .subscribe((data) => {
             //        this._router.navigate(['/timeTable']);
@@ -90,7 +148,32 @@ export class createTimeTable {
     cancel() {
         this._router.navigate(['/timeTable']);
     }
-   
+    onFacultyModel(id) {
+        this.SlotId = id;
+        this.selectFacultyModal.show()
+    }
+    AssignFaculty() {
+        var filterPeriodList = this.periodList.filter(x => x.ID == this.SlotId);
+        console.log(filterPeriodList);
+        console.log(filterPeriodList[0].ID);
+        var id=filterPeriodList[0].ID;
+        if (id) {
+            var teacherCode = (<HTMLInputElement>document.getElementById('Teacher')).value;
+            this.periodList[id].teacher = teacherCode;
+            (<HTMLInputElement>document.getElementById('Teacher')).value = "";
+            
+        }
+    }
+    onDaysModalHidden() {
+
+    }
+    onPeriodModalHidden() {
+
+    }
+    onFacultyModalHidden() {
+
+    }
+
     get CourseId() { return this.timeTableForm.get('CourseId'); }
     get BatchId() { return this.timeTableForm.get('BatchId'); }
     get Name() { return this.timeTableForm.get('Name'); }
